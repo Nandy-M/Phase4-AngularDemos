a9210b411363ad8a090342f7619e76a2091b562c
Nandhini <nandhinimurugesan03@gmail.com>
2022-09-26 12:07:36 +0530

    class 26/09

--- myapp/src/app/ecomm/ecomm.component.ts
+++ myapp/src/app/ecomm/ecomm.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EcommComponent implements OnInit {
   products = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
   onProductAdded(data) {
     this.products.push({
       name: data.name,
